60b731072aa8bd6f13ce43f34b35655680d23097
fluffyikira <tusharnair02@gmail.com>
2022-04-10 22:43:51 +0530

    added deadline element

---
+++ time.js
@@ -0,0 +1,26 @@
+	
+var timeLeft = 5;
+var count;
+
+window.hideLink = function hideLink()
+{
+    document.getElementById("link").style.visibility = "visible";
+    count = setInterval (decrementTimer, 1000);
+    setTimeout (showLink,1000 * timeLeft);
+};
+
+function decrementTimer()
+{
+    timeLeft = timeLeft - 1;
+    document.getElementById("timer").innerHTML = timeLeft + " seconds";
+    if(timeLeft <= 0)
+    {
+        window.clearInterval(count);
+        document.getElementById("timer").style.visibility = "hidden";
+    }
+}
+
+function showLink()
+{
+    document.getElementById("link").style.visibility = "hidden";
+}
